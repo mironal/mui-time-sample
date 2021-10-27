9bfe2a019e2c99bf334be2de3f38d6489d9adc95
mironal <miro.gunp@gmail.com>
2021-10-27 12:21:58 +0900

    普通の import

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  Box,
+  CircularProgress,
+  Container,
+  createTheme,
+  Typography,
+} from "@mui/material";
+import { ThemeProvider } from "@emotion/react";
+import { green, purple } from "@mui/material/colors";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Typography>Test</Typography>
+        <Box>
+          <CircularProgress />
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
 
